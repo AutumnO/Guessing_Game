c401c1befdd58013f8186452e462ac13be3687a5
Autumn Ossello <ao191@humboldt.edu>
2020-03-13 17:15:46 -0700

    Created project, tree/node, filled text file q's

---
+++ GuessingGame/Assets/Scripts/BinaryTree.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinaryTree : MonoBehaviour
+{
+    public TreeNode _root;
+
+    public bool Add(int value)
+    {
+        TreeNode previous = null;
+        TreeNode current = this._root;
+
+        while (current != null)
+        {
+            previous = current;
+            if (value < current.value) //Is new node in left tree? 
+                current = current.left;
+            else if (value > current.value) //Is new node in right tree?
+                current = current.right;
+            else
+            {
+                //Exist same value
+                return false;
+            }
+        }
+
+        TreeNode newNode = new TreeNode();
+        newNode.value = value;
+
+        if (this._root == null)//Tree ise empty
+            this._root = newNode;
+        else
+        {
+            if (value < previous.value)
+                previous.left = newNode;
+            else
+                previous.right = newNode;
+        }
+
+        return true;
+    }
+}
---
+++ GuessingGame/Assets/Scripts/TreeNode.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeNode : MonoBehaviour
+{
+    public TreeNode left { get; set; }
+    public TreeNode right { get; set; }
+    public int value { get; set; }
+}
