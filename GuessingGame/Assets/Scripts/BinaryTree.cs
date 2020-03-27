575612a6647f85e0d1c2c5d3cd90934720fbda2a
Autumn Ossello <ao191@humboldt.edu>
2020-03-27 04:14:15 -0700

    made functional buttons, worked on tree creation

--- GuessingGame/Assets/Scripts/BinaryTree.cs
+++ GuessingGame/Assets/Scripts/BinaryTree.cs
@@ -1,23 +1,50 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class BinaryTree : MonoBehaviour
 {
     public TreeNode _root;
 
-    public TreeNode CreateTree(TreeNode root, string line)
+    public void CreateTree(TreeNode root, StreamReader reader)
     {
-        
-        if (root != null)
-        {
-            root = CreateTree(root.left, line);
-            root = CreateTree(root.right, line);
+        /*reader is called every recursion, need to separate them somehow if using recursion?
+            can't debug because unity freezes when it runs >:(
+    */
+        string line;
+        using (reader)
+        {   
+            line = reader.ReadLine();
+            while (line != null)
+            {
+                TreeNode new_node = gameObject.AddComponent <TreeNode>();
+                new_node.value = line;
+
+                if (root == null)
+                {
+                    root = new_node;
+                }
+                else if (root.left == null)
+                {
+                    root.left = new_node;
+                    if (line[0] != '*')
+                        root = root.left;
+                }
+                else if (root.right == null)
+                {
+                    root.right = new_node;
+                    if (line[0] != '*')
+                        root = root.right;
+                }
+                else
+                {
+                    //move back up to root's parent
+                }
+            }
         }
 
-        root.value = line;
-        return root;
     }
 
     public bool Add(string value, string child, TreeNode root)
--- GuessingGame/Assets/Scripts/GameManager.cs
+++ GuessingGame/Assets/Scripts/GameManager.cs
@@ -7,77 +7,27 @@ using System;
 
 public class GameManager : MonoBehaviour
 {
-    public bool LoadTree(string file_name)
+    public void LoadTree(string file_name)
     {
-        try
-        {
-            BinaryTree guessing_tree = new BinaryTree();
-            TreeNode current = guessing_tree._root;
-            string child = "left";
-            char prev = '0';
-            string line;
-            StreamReader the_reader = new StreamReader(file_name, Encoding.Default);
-
-            using (the_reader)      // while stuff to read
-            {
-                do
-                {
-                    line = the_reader.ReadLine();
-                    if (line != null)
-                    {
-                        // deal with the stuff
-                        current = guessing_tree.CreateTree(current, line);
-
-
-                       /*
-                        if (current == null)
-                        {
-                            guessing_tree._root.value = line;
-                            current = guessing_tree._root;
-                        }
-                        else if (line[0] == '*')
-                        {
-                            if (prev == '*')
-                                child = "right";
-                            else
-                                child = "left";
-
-                            guessing_tree.Add(line, child, current);
-                        }
-                        else
-                        {
-                            guessing_tree.Add(line, child, current);
-                            if (child == "left")
-                                current = current.left;
-                            else if (child == "right")
-                                current = current.right;
-                        }
-
-                        prev = line[0];
-                        */
-                    }
-                }
-
-                while (line != null);
-                {
-                    the_reader.Close();
-                    return true;
-                }
-            }
-
-        }
-
-        catch (Exception e)
-        {
-            Console.WriteLine("{0}\n", e.Message);
-            return false;
-        }
+        BinaryTree guessing_tree = gameObject.AddComponent<BinaryTree>();
+        StreamReader reader = new StreamReader(file_name, Encoding.Default);
+        guessing_tree.CreateTree(guessing_tree._root, reader);
+        reader.Close();
     }
-    
+
+    public void yes()
+    {
+        Debug.Log("Clicked Yes");
+    }
+    public void no()
+    {
+        Debug.Log("Clicked No");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadTree("AnimalTree.txt");
     }
 
     // Update is called once per frame
