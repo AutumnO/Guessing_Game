9c1a071ecaa13aa111e7260ba8df9e49c618b7fb
Autumn Ossello <ao191@humboldt.edu>
2020-03-27 22:13:13 -0700

    Debugged, now edits file, needs input and restart

--- GuessingGame/Assets/Scripts/BinaryTree.cs
+++ GuessingGame/Assets/Scripts/BinaryTree.cs
@@ -1,117 +1,64 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class BinaryTree : MonoBehaviour
 {
     public TreeNode _root;
     public TreeNode _current;
 
-    public void UpdateText(string text)
-    {
-        if (text[0] == '*')
-        {
-            string new_text = text.Substring(1);
-            text = ("Is it a " + new_text);
-        }
-
-        Text question = gameObject.AddComponent<Text>();
-        //q = GameObject.Instantiate<Text>;
-
-        GameObject newText = new GameObject(text.Replace(" ", "-"), typeof(RectTransform));
-        var newTextComp = newText.AddComponent<Text>();
-
-        newTextComp.text = text;
-        newTextComp.alignment = TextAnchor.MiddleCenter;
-        newTextComp.fontSize = 20;
-
-        newText.transform.SetParent(transform);
-    }
-
-    public void Guess(bool answer)
-    {
-        if (!answer)
-        {
-            // CPU LOSE CASE
-            UpdateText("You WON!!! What was your animal?");
-            UpdateText("What is a question that could be used to identify your animal?");
-
-        }
-        else
-        {
-            // CPU WIN CASE
-            UpdateText("You Lost :(");
-        }
-    }
-
-    public void yes()
-    {
-        Debug.Log("Clicked Special Yes");
-        TreeNode b = _current;
-        var c = 0;
-        if (_current.value[0] == '*')    //if leaf node
-        {
-            Guess(true);
-        }
-        TreeNode new_node = TraverseTree(_current, true);
-        UpdateText(new_node.value);
-    }
-
     public void CreateTree(StreamReader reader)
     {
         /*reader is called every recursion, need to separate them somehow if using recursion?
             can't debug because unity freezes when it runs >:(
     */
-        string line;
+        string line = "";
         TreeNode root = _root;
         using (reader)
         {   
-            
             while (reader.EndOfStream == false)
             {
                 line = reader.ReadLine();
-                TreeNode new_node = gameObject.AddComponent<TreeNode>();
-                //GameObject.Instantiate <TreeNode>(new_node);
-                new_node.value = line;
-                bool added = false;
-
-                while (added == false)
+                if (line != "/end/")
                 {
-                    if (root == null)
+                    TreeNode new_node = gameObject.AddComponent<TreeNode>();
+                    new_node.value = line;
+
+                    bool added = false;
+                    while (added == false)
                     {
-                        if (_root == root)
+                        if (root == null)
                         {
-                            _root = new_node;
-                            root = _root;
+                            if (_root == root)
+                            {
+                                _root = new_node;
+                                root = _root;
+                            }
+                            else
+                                root = new_node;
+
+                            added = true;
+                        }
+                        else if (root.left == null)
+                        {
+                            root.left = new_node;
+                            root.left.parent = root;
+                            added = true;
+                            if (line[0] != '*')
+                                root = root.left;
+                        }
+                        else if (root.right == null)
+                        {
+                            root.right = new_node;
+                            root.right.parent = root;
+                            added = true;
+                            if (line[0] != '*')
+                                root = root.right;
                         }
                         else
-                            root = new_node;
-
-                        added = true;
-                    }
-                    else if (root.left == null)
-                    {
-                        root.left = new_node;
-                        root.left.parent = root;
-                        added = true;
-                        if (line[0] != '*')
-                            root = root.left;
-                    }
-                    else if (root.right == null)
-                    {
-                        root.right = new_node;
-                        root.right.parent = root;
-                        added = true;
-                        if (line[0] != '*')
-                            root = root.right;
-                    }
-                    else
-                    {
-                        //move back up to root's parent
-                        root = root.parent;
+                        {
+                            //move back up to root's parent
+                            root = root.parent;
+                        }
                     }
                 }
             }
@@ -119,51 +66,31 @@ public class BinaryTree : MonoBehaviour
 
     }
 
-    public TreeNode TraverseTree(TreeNode prev_q, bool answer)
+    public TreeNode TraverseTree(TreeNode previous, bool answer)
     {
         if (answer)
         {
-            _current = prev_q.right;
-            return prev_q.right;
+            _current = previous.right;
+            return previous.right;
         }
         else
         {
-            _current = prev_q.left;
-            return prev_q.left;
+            _current = previous.left;
+            return previous.left;
         }
     }
 
-
-    public void Go()
+    public TreeNode LocateEnd(TreeNode end_node)
     {
-        //BinaryTree guessing_tree = gameObject.AddComponent<BinaryTree>();
-        
-        _current = _root;
-        UpdateText(_root.value);
-    }
-
-    public bool Add(string value, string child, TreeNode root)
-    {
-        TreeNode parent = root;
-
-        TreeNode new_node = new TreeNode();
-        new_node.value = value;
-        if (child == "left")
+        if (end_node.right != null)
         {
-            parent.left = new_node;
-            return true;
+            return end_node = LocateEnd(end_node.right);
         }
-        else if (child == "right")
+        else if (end_node.left != null)
         {
-            parent.right = new_node;
-            return true;
-        }
-        else if (root == null)
-        {
-            this._root = new_node;
-            return true;
+            return end_node = LocateEnd(end_node.left);
         }
         else
-            return false;
+            return end_node;
     }
 }
--- GuessingGame/Assets/Scripts/GameManager.cs
+++ GuessingGame/Assets/Scripts/GameManager.cs
@@ -1,21 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Text;
 using System.IO;
 using UnityEngine;
-using System;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public BinaryTree _guessing_tree;
-    public BinaryTree LoadTree(string file_name, BinaryTree guessing_tree)
+    public Text _text;
+    public char _prev;
+    public string _file_name = "AnimalTree.txt";
+    public BinaryTree LoadTree()
     {
-        //BinaryTree guessing_tree = gameObject.GetComponent<BinaryTree>();
-        StreamReader reader = new StreamReader(file_name, Encoding.Default);
-        guessing_tree.CreateTree(reader);
+        StreamReader reader = new StreamReader(_file_name, Encoding.Default);
+        _guessing_tree.CreateTree(reader);
         reader.Close();
-        return guessing_tree;
+        _guessing_tree._current = _guessing_tree._root;
+        return _guessing_tree;
     }
 
     public void UpdateText(string text)
@@ -25,76 +25,93 @@ public class GameManager : MonoBehaviour
             string new_text = text.Substring(1);
             text = ("Is it a " + new_text);
         }
-
-        Text question = gameObject.AddComponent <Text>();
-        //q = GameObject.Instantiate<Text>;
-
-        GameObject newText = new GameObject(text.Replace(" ", "-"), typeof(RectTransform));
-        var newTextComp = newText.AddComponent<Text>();
-
-        newTextComp.text = text;
-        newTextComp.alignment = TextAnchor.MiddleCenter;
-        newTextComp.fontSize = 20;
-
-        newText.transform.SetParent(transform);
+        _text.text = text;
     }
 
-    public void Guess(bool answer)
+    public void Guess(bool answer)  //child is the location the new question should be relative to previous question
     {
         if (!answer)
         {
             // CPU LOSE CASE
             UpdateText("You WON!!! What was your animal?");
             UpdateText("What is a question that could be used to identify your animal?");
+            string new_question = "/////////////////?";
+            string new_answer = '*' + "//////////////////////" + '?';
 
+            TreeNode previous = _guessing_tree._current;
+            string line;
+            string file = "";
+
+            StreamReader reader = new StreamReader(_file_name);
+            while (reader.EndOfStream == false)
+            {
+                line = reader.ReadLine();                   //read line
+                if (file != "")                             //add new lines in between
+                    file += "\n";
+                if (line == previous.value)                    //if line is the previous preorder node
+                {
+                    file += new_question;
+                    file += "\n" + line;
+                    file += "\n" + new_answer;
+                }
+                else
+                    file += line;
+            }
+            reader.Close();
+
+            StreamWriter writer = new StreamWriter(_file_name);
+            writer.Write(file);
+            writer.Close();
         }
         else
         {
             // CPU WIN CASE
-            UpdateText("You Lost :(");
+            UpdateText("I Guessed Correct!! Would you like to try again?");
         }
     }
 
     public void yes()
     {
-        Debug.Log("Clicked Yes");
-        BinaryTree guessing_tree = gameObject.GetComponent<BinaryTree>();
-        if (guessing_tree._current.value[0] == '*')    //if leaf node
+        if (_guessing_tree._current.value[0] == '*')    //if leaf node
         {
-            Guess(true);
+            if (_prev == 'Y')                           //if previous answer was yes, new question is right child
+                Guess(true);
+            else                                        //if prev answer was no, new question is left child
+                Guess(true);        
         }
-        TreeNode new_node = guessing_tree.TraverseTree(guessing_tree._current, true);
-        UpdateText(new_node.value);
+        else
+        {
+            TreeNode previous = _guessing_tree._current;
+            TreeNode new_node = _guessing_tree.TraverseTree(_guessing_tree._current, true);
+            if (new_node != null)
+                UpdateText(new_node.value);
+        }
+        _prev = 'Y';
     }
-    public void no(BinaryTree guessing_tree)
+    public void no()
     {
-        Debug.Log("Clicked No");
-        //BinaryTree guessing_tree = gameObject.GetComponent<BinaryTree>();
-        TreeNode babe = guessing_tree._root;
-        if (guessing_tree._current.value[0] == '*')    //if leaf node
+        if (_guessing_tree._current.value[0] == '*')    //if leaf node
         {
-            Guess(false);
+            if (_prev == 'Y')                           //if previous answer was yes, new question is right child
+                Guess(false);
+            else                                        //if previous answer was no, new question is left child
+                Guess(false);
         }
-        TreeNode new_node = guessing_tree.TraverseTree(guessing_tree._current, false);
-        UpdateText(new_node.value);
+        else
+        {
+            TreeNode previous = _guessing_tree._current;
+            TreeNode new_node = _guessing_tree.TraverseTree(_guessing_tree._current, false);
+
+            if (new_node != null)
+                UpdateText(new_node.value);
+        }
+        _prev = 'N';
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        BinaryTree guessing_tree = null;
-        GameObject.Instantiate<BinaryTree>(guessing_tree);
-        guessing_tree = LoadTree("AnimalTree.txt", guessing_tree);
-
-        /*BinaryTree guessing_tree = gameObject.AddComponent<BinaryTree>();
-        guessing_tree = LoadTree("AnimalTree.txt");
-        guessing_tree._current = guessing_tree._root;
-        UpdateText(guessing_tree._root.value);*/
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _guessing_tree = LoadTree();
+        UpdateText(_guessing_tree._root.value);
     }
 }
